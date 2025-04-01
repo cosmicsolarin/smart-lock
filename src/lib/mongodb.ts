ab99341ed0031a2d1df9871167003f7a15341801
cosmicsolarin <prajyu4pes@gmail.com>
2025-04-02 02:48:17 +0530

    DB updates

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,13 +1,27 @@
 import mongoose from "mongoose";
+
 const { MONGODB_URI } = process.env;
+
+let isConnected = false; // Track the connection status
+
 export const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection.");
+    return;
+  }
+
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI as string);
-    if (connection.readyState === 1) {
-      return Promise.resolve(connection);
-    }
+    const { connection } = await mongoose.connect(MONGODB_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false, // Disable buffering of commands before a connection is established
+    });
+
+    isConnected = connection.readyState === 1; // 1 means connected
+
+    console.log("Database connected successfully.");
   } catch (error) {
-    console.error(error);
-    return Promise.reject(error);
+    console.error("Database connection error:", error);
+    throw new Error("Failed to connect to database");
   }
 };
